31d8512b51d56a30aeb3da72ccba122779969b77
Laszlo Szalai <l.szalai@super-staffing.com>
2023-05-14 16:58:10 +0800

    feat: Add game logic and UI to App component . This commit adds the game logic and user interface to the App component. The component uses the fetchQuizQuestions function to fetch a set of questions from the Open Trivia Database API, and then displays each question in a QuestionCard component. The user can select an answer for each question, and the component keeps track of the user s score and the number of questions answered. When the game is over, the component displays the final score and a button to start a new game.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { fetchQuizQuestions } from './API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // types
-import { QuestionsState, Difficulty } from './API';
+import { QuestionsState, Difficulty } from './types';
 
 const TOTAL_QUESTIONS = 10;
 
@@ -12,10 +12,59 @@ const App: React.FC = () => {
   const [score, setScore] = useState(0);
   const [currentAnswer, setCurrentAnswer] = useState("");
   const [isGameOver, setIsGameOver] = useState(false);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
 
-  return <>
-    
-  </>
-}
+  const startTrivia = async () => {
+    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    setQuestions(newQuestions);
+  };
+
+  const checkAnswer = (answer: string) => {
+    if (!isGameOver) {
+      const isCorrect = questions[answerCounter].correct_answer === answer;
+      if (isCorrect) {
+        setScore(prev => prev + 1);
+      }
+      setCurrentAnswer(answer);
+      setIsGameOver(answerCounter === TOTAL_QUESTIONS - 1);
+      setAnswerCounter(prev => prev + 1);
+    }
+  };
+
+  const nextQuestion = () => {
+    setCurrentAnswer("");
+    if (answerCounter < TOTAL_QUESTIONS - 1) {
+      setAnswerCounter(prev => prev + 1);
+    } else {
+      setIsGameOver(true);
+    }
+  };
+
+  return (
+    <>
+      <h1>React Quiz</h1>
+      {isGameOver || answerCounter === TOTAL_QUESTIONS ? (
+        <div>
+          <p>Game Over! Your final score is {score} out of {TOTAL_QUESTIONS}.</p>
+          <button onClick={startTrivia}>Start Again</button>
+        </div>
+      ) : (
+        <QuestionCard
+          questionNumber={answerCounter + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[answerCounter].question}
+          answers={questions[answerCounter].answers}
+          userAnswer={currentAnswer}
+          callback={checkAnswer}
+        />
+      )}
+      {!isGameOver && !loading && currentAnswer !== "" ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
+    </>
+  );
+};
 
 export default App;
